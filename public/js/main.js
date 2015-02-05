2cb9e4ca2fbcbaa74cc75e7f944d72707524c9cf
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-05 11:33:08 -0600

    created a $.get call, tests were successful

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+function getStock(symbol, cb) {
+  var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol;
+
+  $.get(url, function (res) {
+    return cb(res);
+  }, 'jsonp');
+}
+
+
+
+function hello() {
+  return 'world';
+}
+
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -6,3 +6,24 @@ describe('test suite', function () {
     false.should.be.false;
   });
 });
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
+  });
+});
+
+describe('getStock', function () {
+  it('should return a stock object', function (done) {
+    getStock('AAPL', function(stock) {
+      stock.Name.should.equal('Apple Inc');
+      done();
+    });
+  });
+   it('should return a stock object', function(done) {
+    getStock('MSFT', function(stock) {
+      stock.Name.should.equal('Microsoft Corp');
+      done();
+    });
+  });
+});
