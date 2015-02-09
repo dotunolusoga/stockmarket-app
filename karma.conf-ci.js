5f3cd373be547d2e267fff515e3fe25d04e54fd3
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-09 14:17:54 -0600

    corrected syntax error in karma config file

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -85,7 +85,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Firefox', 'SL_1E10'],
+    browsers: ['PhantomJS', 'Firefox', 'SL_IE10'],
 
 
     // Continuous Integration mode
